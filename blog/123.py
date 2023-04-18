57c6f21923a94c915a881a51914c64ebd3ebe37e
Maksimov <amaksimov744@gmail.com>
2023-04-18 21:15:35 +0300

    Second homework for Flask Course.

---
+++ blog/123.py
@@ -0,0 +1,16 @@
+ARTICLES = {
+    1: {
+        "title": "First title",
+        "description": "Some description",
+        "text": "Some text",
+        "author": 1
+    },
+    2: {
+        "title": "Second title",
+        "description": "Another description",
+        "text": "Another some text",
+        "author": 2
+    }
+}
+for a in ARTICLES:
+    print(a)
