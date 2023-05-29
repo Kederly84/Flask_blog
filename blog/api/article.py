402e8ebb93504cd72d0311f93c35a359eee44fb8
Maksimov <amaksimov744@gmail.com>
2023-05-29 21:02:55 +0300

    Completed all tasks for the ten homework.

--- blog/api/article.py
+++ blog/api/article.py
@@ -1,10 +1,18 @@
+from combojsonapi.event.resource import EventsResource
 from flask_combo_jsonapi import ResourceDetail, ResourceList
-from blog.schemas import ArticleSchema
+
 from blog.database import db
 from blog.models import Article
+from blog.schemas import ArticleSchema
+
+
+class ArticleListEvents(EventsResource):
+    def event_get_count(self):
+        return {"count": Article.query.count()}
 
 
 class ArticleList(ResourceList):
+    events = ArticleListEvents
     schema = ArticleSchema
     data_layer = {
         "session": db.session,
